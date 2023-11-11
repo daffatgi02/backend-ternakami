7a976b79c4b2bae8c509c38939ceca17f1a54845
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-11-11 17:06:10 +0700

    Update app.py refactor,fix any

--- backend-machinelearning/app.py
+++ backend-machinelearning/app.py
@@ -6,23 +6,29 @@ from PIL import Image
 from io import BytesIO
 from flask import Flask, request, jsonify
 from google.cloud import storage
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+# Menggunakan variabel lingkungan untuk konfigurasi
+SERVICE_ACCOUNT_KEY_PATH = os.getenv('SERVICE_ACCOUNT_KEY_PATH', 'key.json')
+BUCKET_NAME = os.getenv('BUCKET_NAME', 'appengine-cloudstorage')
+IMAGE_FOLDER = os.getenv('IMAGE_FOLDER', 'image')
 
-# Key JSON Google service account
-service_account_key_path = 'key.json'
-model_bucket = 'appengine-cloudstorage'
+def load_model_from_gcs(service_account_key_path, model_blob):
+    try:
+        storage_client = storage.Client.from_service_account_json(service_account_key_path)
+        bucket = storage_client.bucket(BUCKET_NAME)
+        blob = bucket.blob(model_blob)
+        model_content = blob.download_as_bytes()
+        model = tf.lite.Interpreter(model_content=model_content)
+        model.allocate_tensors()
+        input_details = model.get_input_details()
+        output_details = model.get_output_details()
+        return model, input_details, output_details
+    except Exception as e:
+        raise RuntimeError(f"Error loading model: {e}")
 
-def load_model_from_gcs(model_blob):
-    storage_client = storage.Client.from_service_account_json(service_account_key_path)
-    bucket = storage_client.bucket(model_bucket)
-    blob = bucket.blob(model_blob)
-    model_content = blob.download_as_bytes()  # Download as binary data
-    model = tf.lite.Interpreter(model_content=model_content)
-    model.allocate_tensors()
-    input_details = model.get_input_details()
-    output_details = model.get_output_details()
-    return model, input_details, output_details
 
 def preprocess_image(image, input_shape):
     image = image.resize((input_shape[1], input_shape[2]))
@@ -31,53 +37,58 @@ def preprocess_image(image, input_shape):
     image = image.astype(np.float32) / 255.0
     return image
 
-def predict_image(image, interpreter, input_details, output_details):
+def predict_image(image, interpreter, input_details, output_details, animal_type):
     input_shape = input_details[0]['shape']
     image = preprocess_image(image, input_shape)
     interpreter.set_tensor(input_details[0]['index'], image)
     interpreter.invoke()
     output = interpreter.get_tensor(output_details[0]['index'])
-    predicted_class = "Mata Terjangkit Penyakit Pinkeye" if output[0][0] < 0.5 else "Mata Hewan Kamu Sehat!"
+    
+    if animal_type == "sapi":
+        predicted_class = "Mata sapi kamu Terjangkit Penyakit Pinkeye" if output[0][0] < 0.5 else "Mata sapi kamu Sehat!"
+    elif animal_type == "kambing":
+        predicted_class = "Mata kambing kamu Terjangkit Penyakit Pinkeye" if output[0][0] < 0.5 else "Mata kambing kamu Sehat!"
+    else:
+        predicted_class = "Unknown Animal Type"
+    
     probability = output[0][0]
     return predicted_class, probability
 
-def upload_image_to_gcs(image, bucket_name, image_folder):
-    filename = str(uuid.uuid4()) + '.jpg'
-    storage_client = storage.Client.from_service_account_json(service_account_key_path)
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(f"{image_folder}/{filename}")
-    image_bytes = BytesIO()
-    image.save(image_bytes, format='JPEG')
-    image_bytes.seek(0)
-    blob.upload_from_file(image_bytes, content_type='image/jpeg')
-    return blob.public_url
+
+def upload_image_to_gcs(image):
+    try:
+        filename = str(uuid.uuid4()) + '.jpg'
+        storage_client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_KEY_PATH)
+        bucket = storage_client.bucket(BUCKET_NAME)
+        blob = bucket.blob(f"{IMAGE_FOLDER}/{filename}")
+        image_bytes = BytesIO()
+        image.save(image_bytes, format='JPEG')
+        image_bytes.seek(0)
+        blob.upload_from_file(image_bytes, content_type='image/jpeg')
+        return blob.public_url
+    except Exception as e:
+        raise RuntimeError(f"Error uploading image: {e}")
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
+    if 'image' not in request.files or 'type' not in request.form:
+        return jsonify({'error': 'No image or type specified'})
+
+    image_file = request.files['image']
+    animal_type = request.form['type']
+    model_blob = f"model_{animal_type}.tflite"
+
     try:
-        if 'image' not in request.files:
-            response = jsonify({'error': 'No image uploaded'})
-            response.status_code = 400  # Bad Request
-            return response
-
-        image_file = request.files['image']
         image = Image.open(image_file)
-
-        model_blob = 'model_sapi.tflite'  # Change this to 'model_kambing.tflite' for the kambing model
-
-        interpreter, input_details, output_details = load_model_from_gcs(model_blob)
-
-        image_url = upload_image_to_gcs(image, model_bucket, 'image')
-
-        predicted_class, probability = predict_image(image, interpreter, input_details, output_details)
-
+        interpreter, input_details, output_details = load_model_from_gcs(SERVICE_ACCOUNT_KEY_PATH, model_blob)
+        image_url = upload_image_to_gcs(image)
+        predicted_class, probability = predict_image(image, interpreter, input_details, output_details, animal_type)
         result = {'class': predicted_class, 'probability': float(probability), 'image_url': image_url}
-        return jsonify(result), 200  # Status code 200 for success
+        return jsonify(result)
     except Exception as e:
-        response = jsonify({'error': 'Terjadi Kesalahan di server'})
-        response.status_code = 500  # Internal Server Error
-        return response
-        
+        return jsonify({'error': str(e)})
+
+
 @app.route('/', methods=['GET'])
 def api_status():
     return jsonify({'message': 'API is active'})
